b3f702d8d2d47145a3c95da0213bea9be2f1671d
yundoll <mar0722@naver.com>
2026-06-05 15:50:16 +0900

    refactor: 장바구니 페이지 콘텐츠 분리

--- client/src/pages/CartPage/CartPage.tsx
+++ client/src/pages/CartPage/CartPage.tsx
@@ -1,13 +1,10 @@
-import Cart from "../../components/Cart/Cart";
-import CartEmpty from "../../components/CartEmpty/CartEmpty";
-import CartError from "../../components/CartError/CartError";
-import CartLoading from "../../components/CartLoading/CartLoading";
 import useCartQuery from "../../hooks/useCartQuery";
 import ShippingFeePolicy from "../../domains/shipping/model";
 import {
   DEFAULT_SHIPPING_FEE,
   FREE_SHIPPING_THRESHOLD,
 } from "../../domains/shipping/constants";
+import CartContent from "./CartContent";
 
 const shippingFeePolicy = new ShippingFeePolicy(
   FREE_SHIPPING_THRESHOLD,
@@ -17,13 +14,17 @@ const shippingFeePolicy = new ShippingFeePolicy(
 const CartPage = () => {
   const { data, loading, error } = useCartQuery();
 
-  const isEmpty = data.length === 0;
-
-  if (loading) return <CartLoading />;
-  if (error) return <CartError error={error.message} />;
-  if (isEmpty) return <CartEmpty />;
-
-  return <Cart cartItemsList={data} shippingFeePolicy={shippingFeePolicy} />;
+  return (
+    <>
+      <h1>장바구니</h1>
+      <CartContent
+        data={data}
+        loading={loading}
+        error={error}
+        shippingFeePolicy={shippingFeePolicy}
+      />
+    </>
+  );
 };
 
 export default CartPage;
