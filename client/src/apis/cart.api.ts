5a2ce0412798808e56de97e491c02a979fa34c35
yundoll <mar0722@naver.com>
2026-06-05 17:17:27 +0900

    fix: 배포 환경 설정 추가

--- client/src/apis/cart.api.ts
+++ client/src/apis/cart.api.ts
@@ -1,6 +1,6 @@
 import type { CartItemsResponseDto, ErrorResponseDto } from "./cart.api.dto";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL ?? "";
 
 function createApiUrl(path: string) {
   if (API_BASE_URL === "") return path;
